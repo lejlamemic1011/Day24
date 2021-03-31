050daa64389d5884fbb649260ffc3ac1002ca2f3
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-31 20:24:50 +0200

    Add files via upload

---
+++ ex02/src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  setMyStorage = () => {
+    document.cookie = "Year=2021";
+    localStorage.setItem('Paragon', 'yes, but Arena first');
+    sessionStorage.setItem('frontend', 'React');
+  }
+
+  getMyStorage = () => {
+    let myCookieData = document.cookie;
+    let myLocalStorageData = localStorage.getItem('Paragon');
+    let mySessionStorageData = sessionStorage.getItem('frontend');
+    
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.setMyStorage}>Set Storage</button>
+         <button onClick={this.getMyStorage}>Get Storage</button>
+      </div>
+    )
+  }
+}
+
+export default App;
